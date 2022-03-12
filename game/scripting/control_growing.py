7e860d23455fc787752e77df67c6704bd9d9146a
iriskarina <iriskarina120120@gmail.com>
2022-03-12 19:36:05 -0300

    Change the condition

--- game/scripting/control_growing.py
+++ game/scripting/control_growing.py
@@ -58,6 +58,7 @@ class ControlGrowing(Action):
         snake = cast.get_first_actor("snakes")
         #snake.turn_head(self._direction)
 
-        my_list = [10, 25, 35, 50, 65, 80, 95, 105, 115, 125, 140, 155, 170]
-        if  self._count in my_list:
+        #my_list = [10, 25, 35, 50, 65, 80, 95, 105, 115, 125, 140, 155, 170]
+        if  self._count == 10:
             snake = snake.grow_tail(1)
+            self._count = 0

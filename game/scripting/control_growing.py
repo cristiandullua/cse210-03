8fdde34cedb35f1348fa7318dcfa2cf90a603345
iriskarina <iriskarina120120@gmail.com>
2022-03-15 10:33:00 -0300

    Two snakes working.

--- game/scripting/control_growing.py
+++ game/scripting/control_growing.py
@@ -53,14 +53,41 @@ class ControlGrowing(Action):
         if self._keyboard_service.is_key_down('s'):
             #self._direction = Point(0, constants.CELL_SIZE)
             self._count += 1
+        
+
+
+        if self._keyboard_service.is_key_down('j'):
+            #self._direction = Point(-constants.CELL_SIZE, 0)
+            self._count += 1
+
+        
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            #self._direction = Point(constants.CELL_SIZE, 0)
+            self._count += 1
+
+        
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            #self._direction = Point(0, -constants.CELL_SIZE)
+            self._count += 1
+
+        
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            #self._direction = Point(0, constants.CELL_SIZE)
+            self._count += 1
 
         
         snake = cast.get_first_actor("snakes")
+        snake2 = cast.get_first_actor("snake2")
         #snake.turn_head(self._direction)
 
+        
         #my_list = [10, 25, 35, 50, 65, 80, 95, 105, 115, 125, 140, 155, 170]
         if  self._count == 12:
-            score = cast.get_first_actor("scores")
-            score.add_points(1)
+            #score = cast.get_first_actor("scores")
+            
             snake = snake.grow_tail(1)
+            snake2 = snake2.grow_tail(1)
             self._count = 0
--- game/scripting/draw_actors_action.py
+++ game/scripting/draw_actors_action.py
@@ -37,7 +37,7 @@ class DrawActorsAction(Action):
         self._video_service.clear_buffer()
         #self._video_service.draw_actor(food)
         self._video_service.draw_actors(segments)
-        self._video_service.draw_actors(segments2, centered=True )
+        self._video_service.draw_actors(segments2)
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
